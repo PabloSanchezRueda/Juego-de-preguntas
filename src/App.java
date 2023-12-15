ba93cab4574f04320a35dc021b5407285d27c6c9
PabloSanchezRueda <psanchez@itpfp.com>
2023-12-15 13:53:31 +0100

    variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] preguntas = {
+            "¿Cuál es el animal más grande del mundo? (a) Elefante (b) Ballena (c) Hipopótamo",
+            "¿Cuál es el río más largo del mundo? (a) Amazonas (b) Nilo (c) Mississippi",
+            "¿En qué año se lanzó el primer iPhone? (a) 2005 (b) 2007 (c) 2010",
+            "¿Cuál es la capital de Australia? (a) Sydney (b) Canberra (c) Melbourne",
+            "¿Cuál es el color del caballo blanco de Napoleón? (a) Blanco (b) Negro (c) Gris",
+            "¿Cuál es la montaña más alta del mundo? (a) Monte Everest (b) Montaña K2 (c) Montaña Kilimanjaro",
+            "¿Cuál es el país más grande del mundo por superficie? (a) Rusia (b) Canadá (c) Estados Unidos",
+            "¿Cuál es el metal más valioso del mundo? (a) Rodio (b) Oro (c) Platino",
+            "¿Cuál es el número atómico del oxígeno? (a) 8 (b) 16 (c) 32",
+            "¿Cuál es el inventor de la bombilla eléctrica? (a) Thomas Edison (b) Nikola Tesla (c) Isaac Newton"
+        };
+
+        String[] respuestas = {
+            "b",
+            "a",
+            "b",
+            "b",
+            "a",
+            "a",
+            "a",
+            "a",
+            "a",
+            "a"
+        };
+
     }
 }
